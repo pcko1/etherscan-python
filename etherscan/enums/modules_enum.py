1e04210d12403fb06e83743d2cc9c8549b42f1d0
Kotsias, Panagiotis-Christos <panagiotis-christos.kotsias@astrazeneca.com>
2020-09-21 22:32:11 +0000

    Added four modules

---
+++ etherscan/enums/modules_enum.py
@@ -0,0 +1,10 @@
+from dataclass import dataclass
+
+
+@dataclass(frozen=True)
+class ModulesEnum:
+    ACCOUNT: str = "account"
+    CONTRACT: str = "contract"
+    TRANSACTION: str = "transaction"
+    TOKEN: str = "token"
+
