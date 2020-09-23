6e6cb9c24f7da47dbf50d9189f96b6f32c8d9e72
Kotsias, Panagiotis-Christos <panagiotis-christos.kotsias@astrazeneca.com>
2020-09-23 12:42:07 +0000

    Added content parser

---
+++ etherscan/parsing.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class ResponseParser:
+    @staticmethod
+    def _get_content(response: requests.Response):
+        return response.json()
+
+    @staticmethod
+    def get_status(response: requests.Response):
+        c = ResponseParser._get_content(response)
+        return c["status"]
+
+    @staticmethod
+    def get_message(response: requests.Response):
+        c = ResponseParser._get_content(response)
+        return c["message"]
+
+    @staticmethod
+    def get_result(response: requests.Response):
+        c = ResponseParser._get_content(response)
+        return c["result"]
