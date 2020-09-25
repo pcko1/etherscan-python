010e5256aedfb90c17f9b593e1d7328605b24b2e
Kotsias, Panagiotis-Christos <panagiotis-christos.kotsias@astrazeneca.com>
2020-09-25 20:21:11 +0000

    Added gastracker and proxy

--- etherscan/enums/modules_enum.py
+++ etherscan/enums/modules_enum.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 class ModulesEnum:
     ACCOUNT: str = "account"
     CONTRACT: str = "contract"
+    GASTRACKER: str = "gastracker"
+    PROXY: str = "proxy"
     STATS: str = "stats"
     TOKEN: str = "token"
     TRANSACTION: str = "transaction"
