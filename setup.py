db71d69749eaadced4aff34375ba01a36c482bfc
Kotsias, Panagiotis-Christos <panagiotis-christos.kotsias@astrazeneca.com>
2020-09-25 18:05:57 +0000

    Added env and package details

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="etherscan-python",
+    version="1.0.0",
+    description="",
+    url="",
+    author="Panagiotis Kotsias",
+    author_email="kotsias.pan@gmail.com",
+    license="MIT",
+    packages=["etherscan"],
+    install_requires=["requests", "coverage"],
+    zip_safe=False,
+)
