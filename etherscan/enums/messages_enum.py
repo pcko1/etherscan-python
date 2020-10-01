c04b8700be2d7dd327a18a1f60de17edd0b38b4c
Panagiotis Kotsias <kotsias.pan@gmail.com>
2020-10-01 13:15:42 +0200

    Removed unused messages

--- etherscan/enums/messages_enum.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class MessagesEnum:
-    METHOD_DISABLED: str = "This method is disabled for now."
-    INVALID_ADDRESS: str = "This address is invalid."
