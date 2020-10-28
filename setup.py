6101a83a55240ec8fe4e476a65a15ee11df5c284
Kotsias, Panagiotis-Christos <panagiotis-christos.kotsias@astrazeneca.com>
2020-10-28 12:14:12 +0000

    Updated to v2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="etherscan-python",
-    version="1.0.3",
+    version="2.0.0",
     description="A minimal, yet complete, python API for etherscan.io.",
     url="https://github.com/pcko1/etherscan-python",
     author="Panagiotis-Christos Kotsias",
